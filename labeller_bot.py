51e548ab2eec3c46dce491095c03f4653f77167a
diesilveira <dsilveiracupre2014@gmail.com>
2021-10-20 22:19:17 -0300

    next image inmediately

--- labeller_bot.py
+++ labeller_bot.py
@@ -3,7 +3,6 @@ import os
 import random
 import conf
 
-import threading
 
 from telegram import (
     Update,
@@ -20,7 +19,6 @@ from telegram.ext import (
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
 
@@ -28,17 +26,18 @@ def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     update.message.reply_markdown_v2(
-        fr'Hello {user.mention_markdown_v2()}\! send me the word "/image"'
+        fr'{user.mention_markdown_v2()}{conf.GREETING}'
     )
+
+    chat_id = update.message.chat_id
+    send_image(chat_id,update,context)
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('you can get help visiting https://github.com/diesilveira/labeller_img_python_telegram_BOT')
+    update.message.reply_text('you can get help visiting https://github.com/diesilveira/labeller_img_python_telegram_BOT ')
 
 
 def get_image():
-    semaphore = Semaphore(1)
-    semaphore.acquire(timeout=0.5)
     if conf.LOCAL:
         img_id = random.choice(os.listdir(conf.PATH_FOLDER))
         with open('finished.txt', 'r') as f:
@@ -47,7 +46,6 @@ def get_image():
             img_id = random.choice(os.listdir(conf.PATH_FOLDER))
 
         image = os.path.join(conf.PATH_FOLDER, img_id)
-        semaphore.release()
 
     else:
         with open('url_images.txt', 'r') as f:
@@ -59,25 +57,12 @@ def get_image():
         while img_id in finished:
             image = random.choice(lines)
             img_id = image.split(';')[0]
-        semaphore.release()
 
     return [image, img_id]
 
 
-def send_image(update: Update, context: CallbackContext) -> None:
-    """send image to tag."""
-
-    image_path_and_id = get_image()
-    image = image_path_and_id[0]
-    img_id = image_path_and_id[1]
-
-    if conf.LOCAL:
-        with open(image, 'rb') as f:
-            update.message.reply_photo(photo=f)
-    else:
-        update.message.reply_photo(photo=image.split(';')[1])
-
-        keyboard = []
+def generate_keyboard(img_id):
+    keyboard = []
     i = 0
     if len(conf.BUTTONS) % 2 == 1:
         keyboard.append([InlineKeyboardButton(conf.BUTTONS[i], callback_data=img_id + ',' + conf.BUTTONS[i])])
@@ -86,20 +71,36 @@ def send_image(update: Update, context: CallbackContext) -> None:
         keyboard.append([InlineKeyboardButton(conf.BUTTONS[i], callback_data=img_id + ',' + conf.BUTTONS[i]),
                          InlineKeyboardButton(conf.BUTTONS[i + 1], callback_data=img_id + ',' + conf.BUTTONS[i + 1])])
         i += 2
+    return keyboard
 
+def send_image(chat_id, update: Update, context: CallbackContext) -> None:
+    """send image to tag."""
+    image_path_and_id = get_image()
+    image = image_path_and_id[0]
+    img_id = image_path_and_id[1]
+
+    if conf.LOCAL:
+        with open(image, 'rb') as f:
+            context.bot.send_photo(chat_id, photo=f)
+    else:
+        context.bot.send_photo(chat_id, photo=image.split(';')[1])
+
+    keyboard = generate_keyboard(img_id)
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(conf.QUESTION, reply_markup=reply_markup)
+    context.bot.send_message(chat_id, text= conf.QUESTION , reply_markup=reply_markup)
 
 
 def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
-    query.answer()
-    query.edit_message_text(text=f"{conf.CHOSE} {query.data.split(',')[1]}")
-    semaphore = Semaphore(1)
-    semaphore.acquire(timeout=0.5)
+    if query.answer():
+        query.edit_message_text(text=f"{conf.CHOSE} {query.data.split(',')[1]}")
+
+    chat_id = update.callback_query.message.chat.id
+
     with open("log.txt", 'a') as f:
         f.write(query.data + '\n')
 
     with open('finished.txt', 'a') as f:
         f.write(query.data.split(',')[0] + '\n')
-    semaphore.release()
+
+    send_image(chat_id, update, context)
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import labellerBot
+import labeller_bot
 import conf
 
 def main() -> None:
     """Start the bot."""
-    updater = labellerBot.Updater(conf.TOKEN)
+    updater = labeller_bot.Updater(conf.TOKEN, use_context=True)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(labellerBot.CommandHandler("start", labellerBot.start))
-    dispatcher.add_handler(labellerBot.CommandHandler("help", labellerBot.help_command))
-    dispatcher.add_handler(labellerBot.CommandHandler("image", labellerBot.send_image))
+    dispatcher.add_handler(labeller_bot.CommandHandler("start", labeller_bot.start))
+    dispatcher.add_handler(labeller_bot.CommandHandler("help", labeller_bot.help_command))
 
-    updater.dispatcher.add_handler(labellerBot.CallbackQueryHandler(labellerBot.button))
+    updater.dispatcher.add_handler(labeller_bot.CallbackQueryHandler(labeller_bot.button))
 
     # Start the Bot
     updater.start_polling()
